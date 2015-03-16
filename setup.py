4a9bf306e258ca94a5cffa5e981a8faa99a89772
robnagler <github@q33.us>
2015-03-16 16:40:47 -0600

    added pybivio

--- setup.py
+++ setup.py
@@ -1,32 +1,17 @@
-#!/usr/bin/env python
-import distutils.core
-import setuptools.command.test as stct
-import sys
-import pip.req
+# -*- coding: utf-8 -*-
+"""PyBivio setup script
 
-class PyTest(stct.test):
-    def initialize_options(self):
-        self.pytest_args = []
+:copyright: Copyright (c) 2015 Bivio Software, Inc.  All Rights Reserved.
+:license: Apache, see LICENSE for more details.
+"""
 
-    def finalize_options(self):
-        self.test_args = []
-        self.test_suite = True
+from pybivio.b_setup import setup
 
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-install_requires = [str(i.req) for i in pip.req.parse_requirements('requirements.txt')]
-distutils.core.setup(
+setup(
     name='publicprize',
     version='1.0',
-    description='Public Prize',
+    description='Public Prize is a platform for venture challenge contests',
     author='Bivio Software, Inc.',
-    author_email='software@bivio.biz',
-    install_requires=install_requires,
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest}
-    )
+    author_email='pip@bivio.biz',
+    url='http://publicprize.com',
+)
